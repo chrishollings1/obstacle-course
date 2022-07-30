79ab424ed55829d632aed8efd086557026251c9b
DESKTOP-VDVB68J\User <chrisjhollings1@outlook.com>
2022-07-30 13:55:43 +0100

    added code for hits

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -6,7 +6,6 @@ public class ObjectHit : MonoBehaviour
 {
    private void OnCollisionEnter(Collision other) 
    {
-      Debug.Log("OUCH !!!");
       GetComponent<MeshRenderer>().material.color = Color.gray;
       //this gets the mesh renderer material color and changes it to the color after the dot
    }
