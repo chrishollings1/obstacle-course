feb823bcd00e81ab21e54ce03cd0316f9e2dab2b
DESKTOP-VDVB68J\User <chrisjhollings1@outlook.com>
2022-07-31 12:37:59 +0100

    tags

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -6,7 +6,10 @@ public class ObjectHit : MonoBehaviour
 {
    private void OnCollisionEnter(Collision other) 
    {
+      if(other.gameObject.tag == "Player")
+      {
       GetComponent<MeshRenderer>().material.color = Color.gray;
       //this gets the mesh renderer material color and changes it to the color after the dot
+      }
    }
 }
